0ece853bee3f005ff600c898d747a48f3c32fdbd
BrentLevings <brent@otomatika.com>
2025-08-08 23:36:10 +0200

    github ready

--- src/generate_page.py
+++ src/generate_page.py
@@ -9,7 +9,7 @@ def extract_title(markdown: str):
     else:
         raise Exception("No h1 header found!")
     
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, "r") as f:
@@ -22,18 +22,18 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     title = extract_title(markdown)
 
     template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-    
+    template = template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     if not os.path.exists(dest_path):
         os.makedirs(os.path.dirname(dest_path) , exist_ok=True)
 
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path: str, template_path: str, dest_dir_path: str):
+def generate_pages_recursive(dir_path: str, template_path: str, dest_dir_path: str, basepath: str):
     for obj in os.listdir(dir_path):
         # print(obj)
         if obj.endswith(".md"):
-            generate_page(os.path.join(dir_path, obj), template_path, os.path.join(dest_dir_path, obj.replace(".md", ".html")))
+            generate_page(os.path.join(dir_path, obj), template_path, os.path.join(dest_dir_path, obj.replace(".md", ".html")), basepath)
         else:
-            generate_pages_recursive(os.path.join(dir_path, obj), template_path, os.path.join(dest_dir_path, obj))
+            generate_pages_recursive(os.path.join(dir_path, obj), template_path, os.path.join(dest_dir_path, obj), basepath)
         
--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 
 
 def main():
+    try:
+        basepath = sys.argv[0]
+    except Exception:
+        basepath = "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -17,6 +23,6 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
     
     print("Generating new page...")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
